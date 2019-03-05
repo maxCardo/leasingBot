dd66b2472e1c0f26889dca62262960d18e0d612c
Adam <adampoznanski@outlook.com>
2019-03-05 12:51:27 -0500

    wired sms for deploy

--- server/assets/sms.js
+++ server/assets/sms.js
@@ -5,15 +5,14 @@ const client = require('twilio')(config.twlsid, config.twlAuthToken);
 
 
 const sendSMS= (to,body) => {
-  console.log('run send SMS');
-  // client.messages
-  // .create({
-  //   body: body,
-  //   from:'+14124447505',
-  //   to: to
-  // })
-  // .then(message => console.log(message.sid))
-  // .done();
+  client.messages
+  .create({
+    body: body,
+    from:'+14124447505',
+    to: to
+  })
+  .then(message => console.log(message.sid))
+  .done();
 }
 
 module.exports = {sendSMS};
