c1b728e0a14c4b250fe1e3ba052967acb62b8e67
Adam <adampoznanski@outlook.com>
2019-08-20 18:41:10 -0400

    updated flw calandly link to group link

--- server/assets/availability.js
+++ server/assets/availability.js
@@ -1,5 +1,5 @@
 const availabilityLink = {
-  '1500 Fallowfield Avenue': 'https://calendly.com/adampoznanski/1500-fallowfield',
+  '1500 Fallowfield Avenue': 'https://calendly.com/1500-fallowfield/1500fallowfield',
   '1415 Rutherford Avenue #2': 'https://calendly.com/teamrutherford/1415rutherfordshowings'
 }
 
