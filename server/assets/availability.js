2d4d35b8b2aa7d08506fe25529e2863fdf1c5a5d
Adam <adampoznanski@outlook.com>
2019-11-20 15:47:49 -0500

    addeded 3349 bookman to calendly avail

--- server/assets/availability.js
+++ server/assets/availability.js
@@ -1,6 +1,7 @@
 const availabilityLink = {
   '1500 Fallowfield Avenue': 'https://calendly.com/1500-fallowfield/1500fallowfield',
   '1415 Rutherford Avenue #2': 'https://calendly.com/teamrutherford/1415rutherfordshowings'
+  '3349 Bookman Avenue': 'https://calendly.com/3349-bookman-ave/3349-leasing'
 }
 
 
