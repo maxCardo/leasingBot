4cb4b4fea0b09896cb2ef6eb4e08d46654927c62
Adam <adampoznanski@outlook.com>
2020-01-23 22:37:39 -0500

    added swisval ave property

--- server/assets/availability.js
+++ server/assets/availability.js
@@ -1,7 +1,8 @@
 const availabilityLink = {
   '1500 Fallowfield Avenue': 'https://calendly.com/1500-fallowfield/1500fallowfield',
   '1415 Rutherford Avenue #2': 'https://calendly.com/teamrutherford/1415rutherfordshowings',
-  '3349 Bookman Avenue': 'https://calendly.com/3349-bookman-ave/3349-leasing'
+  '3349 Bookman Avenue': 'https://calendly.com/3349-bookman-ave/3349-leasing',
+  '305 East Swissvale Avenue': 'https://calendly.com/305-e-swissvale-avenue/305-e-swissvale-showings'
 }
 
 
